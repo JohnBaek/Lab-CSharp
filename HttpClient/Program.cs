1ba6b2f94780977ca51b1bc65f3b72e7ed358b74
JohnMothani <87628593+JohnMothani@users.noreply.github.com>
2023-06-01 16:34:32 +0900

    - 프로젝트 기초 추가

---
+++ HttpClient/Program.cs
@@ -0,0 +1,15 @@
+﻿HttpClient client = new HttpClient();
+
+
+var response = await client.PostAsync("http://localhost:8082/api/v1/my-working-status/private/to-step-out", new StringContent("eba5db8e-f109-41c5-80cb-149efcbd0fe1", System.Text.Encoding.UTF8, "text/plain"));
+
+// Ensure the response was successful
+response.EnsureSuccessStatusCode();
+
+// Read the response content as a string
+string responseBody = await response.Content.ReadAsStringAsync();
+
+// Process the response as needed
+Console.WriteLine("Response: " + responseBody);
+Console.ReadKey();
+
---
+++ MultiCore/Program.cs
@@ -0,0 +1,31 @@
+﻿
+// Target of test multicore 
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+// Process the array using multiple cores
+Parallel.ForEach(numbers , number =>
+{
+    // Simulation Performence Delay
+    int result = Compute(number);
+    
+    // Show output
+    Console.WriteLine("Result for {0}: {1}", number, result);
+    
+    // Wait for all parallel tasks to complete
+    Parallel.ForEach(numbers, num => Console.WriteLine("Task for {0} completed", num));
+
+    Console.WriteLine("All tasks completed. Press any key to exit.");
+    Console.ReadKey();
+});
+
+
+static int Compute(int number)
+{
+    // Simulate some time-consuming computation
+    Task.Delay(1000).Wait();
+
+    // Perform some computation on the number
+    int result = number * number;
+
+    return result;
+}
